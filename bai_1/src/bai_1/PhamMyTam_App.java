143ada1461daa2991ba97174da5047f1608467a2
PhamtamHumg <125909066+PhamtamHumg@users.noreply.github.com>
2023-02-21 13:23:47 +0700

    first commit
    test

---
+++ bai_1/src/bai_1/PhamMyTam_App.java
@@ -0,0 +1,10 @@
+package bai_1;
+
+public class PhamMyTam_App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pham My Tam hoc Java");
+	}
+
+}
